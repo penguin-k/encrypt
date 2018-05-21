c97cde86cf52a52a98b8f9ff0f308dc529009cc8
penguin-k <32879754+penguin-k@users.noreply.github.com>
2018-05-21 19:00:57 +0100

    Update

--- encrypt.js
+++ encrypt.js
@@ -1,16 +1,19 @@
 //A JS library
-function encrypt(text, key) {
+function encrypt(textraw, keyraw) {
   //console.log("----------------");
   var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!�$%^&_={}:;@'~#<,>./|`� abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!�$%^&_={}:;@'~#<,>./|`� abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!�$%^&_={}:;@'~#<,>./|`� ";
   //var chars = sessionStorage.getItem("charStorage");
-  //var chars = "abcdefghijklmnopqrstuvwxyz"
-  var output = "";
+  //var chars = "abcdefghijklmnopqrstuvwxyz"var output = "";
+  var text = String(textraw);
+  var key = String(keyraw);
   var keyNum = 0;
   var char = "";
   var keychar = "";
   var finalchar = "";
+  console.log(text);
+  console.log(key);
   for (var i = 0; i < text.length; i++) {
-    char = chars.indexOf((text.substring(i, i+1)))+261;
+    char = chars.indexOf((text.substring(i, i+1)))+87;
     keychar = (chars.indexOf(key.substring(keyNum, keyNum+1)));
     finalchar = chars[parseFloat(char)-parseFloat(keychar)];
     output = output + finalchar;
@@ -23,12 +26,14 @@ function encrypt(text, key) {
   return output;
 }
 
-function decrypt(text, key) {
+function decrypt(textraw, keyraw) {
   //console.log("----------------");
   //var text = document.getElementById("text").value;
   //var key = document.getElementById("key").value;
   var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!�$%^&_={}:;@'~#<,>./|`� abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!�$%^&_={}:;@'~#<,>./|`� abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!�$%^&_={}:;@'~#<,>./|`� ";
   //var chars = sessionStorage.getItem("charStorage");
+  var text = String(textraw);
+  var key = String(keyraw);
   var output = "";
   var keyNum = 0;
   var char = "";
@@ -39,7 +44,7 @@ function decrypt(text, key) {
     //console.log("char "+char);
     //keychar = chars.search(key.substring(keyNum, KeyNum+1));
     //document.getElementById("output").innerHTML = key.substring(keyNum, keyNum+1);
-    keychar = (chars.indexOf(key.substring(keyNum, keyNum+1))+261);
+    keychar = (chars.indexOf(key.substring(keyNum, keyNum+1))+87);
     //console.log("keychar "+keychar);
     //console.log("char - keychar "+parseFloat(char)-parseFloat(keychar));
     finalchar = chars[parseFloat(char)+parseFloat(keychar)];
